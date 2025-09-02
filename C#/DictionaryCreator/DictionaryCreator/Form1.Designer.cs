d601c8f1ae2d3ae08d429a43405a816b658ffb79
Morteza Maghrebi <M.Maghrebi@argtelecom.com>
2025-09-02 14:12:29 +0330

    add 22000 words to my_dictionary.txt

---
+++ C#/DictionaryCreator/DictionaryCreator/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace DictionaryCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bOpenDictionary = new System.Windows.Forms.Button();
+            this.rtxmy_dictionary = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rtx_not_in_dictionary_words = new System.Windows.Forms.RichTextBox();
+            this.rtx_allwords = new System.Windows.Forms.RichTextBox();
+            this.bOpenAllWords = new System.Windows.Forms.Button();
+            this.bExtract150toDeepseek = new System.Windows.Forms.Button();
+            this.bExtractNewWords = new System.Windows.Forms.Button();
+            this.ldeepseek = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bOpenDictionary
+            // 
+            this.bOpenDictionary.Location = new System.Drawing.Point(80, 6);
+            this.bOpenDictionary.Name = "bOpenDictionary";
+            this.bOpenDictionary.Size = new System.Drawing.Size(93, 25);
+            this.bOpenDictionary.TabIndex = 0;
+            this.bOpenDictionary.Text = "Open Dictionary";
+            this.bOpenDictionary.UseVisualStyleBackColor = true;
+            this.bOpenDictionary.Click += new System.EventHandler(this.bOpenDictionary_Click);
+            // 
+            // rtxmy_dictionary
+            // 
+            this.rtxmy_dictionary.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rtxmy_dictionary.Location = new System.Drawing.Point(0, 50);
+            this.rtxmy_dictionary.Name = "rtxmy_dictionary";
+            this.rtxmy_dictionary.Size = new System.Drawing.Size(258, 390);
+            this.rtxmy_dictionary.TabIndex = 1;
+            this.rtxmy_dictionary.Text = "";
+            this.rtxmy_dictionary.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 440);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(813, 10);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ldeepseek);
+            this.panel1.Controls.Add(this.bExtractNewWords);
+            this.panel1.Controls.Add(this.bExtract150toDeepseek);
+            this.panel1.Controls.Add(this.bOpenAllWords);
+            this.panel1.Controls.Add(this.bOpenDictionary);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(813, 50);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // rtx_not_in_dictionary_words
+            // 
+            this.rtx_not_in_dictionary_words.Dock = System.Windows.Forms.DockStyle.Right;
+            this.rtx_not_in_dictionary_words.Location = new System.Drawing.Point(528, 50);
+            this.rtx_not_in_dictionary_words.Name = "rtx_not_in_dictionary_words";
+            this.rtx_not_in_dictionary_words.Size = new System.Drawing.Size(285, 390);
+            this.rtx_not_in_dictionary_words.TabIndex = 4;
+            this.rtx_not_in_dictionary_words.Text = "";
+            // 
+            // rtx_allwords
+            // 
+            this.rtx_allwords.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtx_allwords.Location = new System.Drawing.Point(258, 50);
+            this.rtx_allwords.Name = "rtx_allwords";
+            this.rtx_allwords.Size = new System.Drawing.Size(270, 390);
+            this.rtx_allwords.TabIndex = 5;
+            this.rtx_allwords.Text = "";
+            // 
+            // bOpenAllWords
+            // 
+            this.bOpenAllWords.Location = new System.Drawing.Point(337, 5);
+            this.bOpenAllWords.Name = "bOpenAllWords";
+            this.bOpenAllWords.Size = new System.Drawing.Size(93, 25);
+            this.bOpenAllWords.TabIndex = 1;
+            this.bOpenAllWords.Text = "Open All words";
+            this.bOpenAllWords.UseVisualStyleBackColor = true;
+            this.bOpenAllWords.Click += new System.EventHandler(this.bOpenAllWords_Click);
+            // 
+            // bExtract150toDeepseek
+            // 
+            this.bExtract150toDeepseek.Location = new System.Drawing.Point(661, 6);
+            this.bExtract150toDeepseek.Name = "bExtract150toDeepseek";
+            this.bExtract150toDeepseek.Size = new System.Drawing.Size(140, 25);
+            this.bExtract150toDeepseek.TabIndex = 2;
+            this.bExtract150toDeepseek.Text = "Extract 150 to Deepseek";
+            this.bExtract150toDeepseek.UseVisualStyleBackColor = true;
+            this.bExtract150toDeepseek.Click += new System.EventHandler(this.bExtract150toDeepseek_Click);
+            // 
+            // bExtractNewWords
+            // 
+            this.bExtractNewWords.Location = new System.Drawing.Point(528, 6);
+            this.bExtractNewWords.Name = "bExtractNewWords";
+            this.bExtractNewWords.Size = new System.Drawing.Size(127, 25);
+            this.bExtractNewWords.TabIndex = 3;
+            this.bExtractNewWords.Text = "Extract New Words";
+            this.bExtractNewWords.UseVisualStyleBackColor = true;
+            this.bExtractNewWords.Click += new System.EventHandler(this.bExtractNewWords_Click);
+            // 
+            // ldeepseek
+            // 
+            this.ldeepseek.AutoSize = true;
+            this.ldeepseek.Location = new System.Drawing.Point(661, 34);
+            this.ldeepseek.Name = "ldeepseek";
+            this.ldeepseek.Size = new System.Drawing.Size(35, 13);
+            this.ldeepseek.TabIndex = 4;
+            this.ldeepseek.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 450);
+            this.Controls.Add(this.rtx_allwords);
+            this.Controls.Add(this.rtx_not_in_dictionary_words);
+            this.Controls.Add(this.rtxmy_dictionary);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "My_Dictionary";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bOpenDictionary;
+        private System.Windows.Forms.RichTextBox rtxmy_dictionary;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox rtx_not_in_dictionary_words;
+        private System.Windows.Forms.RichTextBox rtx_allwords;
+        private System.Windows.Forms.Button bExtract150toDeepseek;
+        private System.Windows.Forms.Button bOpenAllWords;
+        private System.Windows.Forms.Button bExtractNewWords;
+        private System.Windows.Forms.Label ldeepseek;
+    }
+}
+
---
+++ C#/DictionaryCreator/DictionaryCreator/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DictionaryCreator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bOpenDictionary_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                rtxmy_dictionary.Text=System.IO.File.ReadAllText(op.FileName);
+            }
+        }
+
+        private void bOpenAllWords_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                rtx_allwords.Text = System.IO.File.ReadAllText(op.FileName);
+            }
+        }
+
+        private void bExtract150toDeepseek_Click(object sender, EventArgs e)
+        {
+            string prompt = "فرض کن یک زبان شناس هستی که به دیکشنری های آکسفورد و کمبریج دسترسی داری و میخوای با لیست کلماتی که من بهت میدم فایل آموزش لغات زبان انگلیسی درست کنی. فایلی که من میدم در هر خط یک کلمه داره، تو در هر خط، کلمه رو به این فرمت مینویسی:\r\nword#meaning in persian#example in sentence#synonym#CEFR level#pronounciation in american#translation of example in persian\r\nتو فقط فایل خروجی رو میدی و هیچ توضیحات دیگه ای نمیدی\r\nمعنی فارسی و ترجمه جمله به فارسی نباید شامل کلمات غیر فارسی باشد\r\nاز اونجایی که این پرامپت طولانیه من میرم و نیم ساعت دیگه بر می میگردم، میخوام هیچ اینتراپتی وسطش ندی یا سوالی نپرسی یا سوال میوخوای ادامه بدی رو نپرسی و خودت ادامه بدی\r\nWords:\r\n";
+            int startindex = lastindex_sent_deepseek;
+            int stopindex = 0;
+            if(new_words!=null)
+            {
+               
+                for(int i=lastindex_sent_deepseek;i<Math.Min(lastindex_sent_deepseek+150,new_words.Count());i++)
+                {
+                    prompt += new_words[i] + "\r\n";
+                    stopindex = i;
+                }
+                lastindex_sent_deepseek = stopindex + 1;
+                System.IO.File.WriteAllText("lastindex", lastindex_sent_deepseek + "");
+                OpenBrowser("https://gemini.google.com/app?is_sa=1&is_sa=1&android-min-version=301356232&ios-min-version=322.0&campaign_id=bkws&utm_source=sem&utm_source=google&utm_medium=paid-media&utm_medium=cpc&utm_campaign=bkws&utm_campaign=2024enGB_gemfeb&pt=9008&mt=8&ct=p-growth-sem-bkws&gclsrc=aw.ds&gad_source=1&gad_campaignid=22921538072&gbraid=0AAAAApk5BhkUWTASFVSBF0c3iiTCiuwVu&gclid=CjwKCAjwk7DFBhBAEiwAeYbJsbHeEYRJX_4YW3pRoXuZ2ru24Tnblo7ZtQA8O6NUEABmD6g6Usv9MRoCNM8QAvD_BwE");
+                Clipboard.SetText(prompt);
+                System.Threading.Thread.Sleep(5000);
+                SendKeys.Send("^v");
+                System.Threading.Thread.Sleep(500);
+                SendKeys.Send("{ENTER}");
+            }
+            ldeepseek.Text = startindex + " to " + stopindex;
+        }
+
+        
+
+        List<string> my_words = new List<string>();
+        List<string> all_words = new List<string>();
+        List<string> new_words;
+        int lastindex_sent_deepseek = 0;
+        private void bExtractNewWords_Click(object sender, EventArgs e)
+        {
+            string[] lines = rtxmy_dictionary.Lines;
+            for (int i=0;i< lines.Length;i++)
+            {
+                string word = lines[i].Split('#')[0].ToLower();
+                my_words.Add(word);
+                progressBar1.Value = i * 100 / lines.Length;
+                if(i%10==0) Application.DoEvents();
+                
+            }
+            lines = rtx_allwords.Lines;
+            ldeepseek.Text = my_words.Count() + "";
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string word = lines[i].Trim();
+                if(word.Length>=3) all_words.Add(word);
+                progressBar1.Value = i * 100 / lines.Length;
+                if (i % 10 == 0) Application.DoEvents();
+            }
+            new_words = all_words.Where(x => !my_words.Contains(x)).ToList();
+            ldeepseek.Text += " "+new_words.Count() + "";
+            string new_words_str = "";
+            foreach (string word in new_words)
+            {
+                new_words_str += word + "\r\n";
+            }
+            rtx_not_in_dictionary_words.Text = new_words_str;
+        }
+
+        public static void OpenBrowser(string url)
+        {
+            // Use the OS-specific command to open a web browser
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            });//try
+            //{
+            //    Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            //}
+            //catch (System.ComponentModel.Win32Exception)
+            //{ 
+            //     Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            //}
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                lastindex_sent_deepseek = int.Parse(System.IO.File.ReadAllText("lastindex"));
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
