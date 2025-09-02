5b3b6b103d57a4500112c408de99ad01decdb985
MortezaMaghrebi <Maghrebimorteza@gmail.com>
2025-09-03 00:08:04 +0330

    add dictionaychecker

---
+++ C#/DictionaryCheker/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.rtxWithouExampleFa = new System.Windows.Forms.RichTextBox();
+            this.rxtOnlyExample = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(206, 21);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(263, 324);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(475, 21);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(254, 324);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = resources.GetString("richTextBox2.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 371);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(735, 21);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(254, 324);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = resources.GetString("richTextBox3.Text");
+            // 
+            // rtxWithouExampleFa
+            // 
+            this.rtxWithouExampleFa.Location = new System.Drawing.Point(202, 379);
+            this.rtxWithouExampleFa.Name = "rtxWithouExampleFa";
+            this.rtxWithouExampleFa.Size = new System.Drawing.Size(257, 193);
+            this.rtxWithouExampleFa.TabIndex = 4;
+            this.rtxWithouExampleFa.Text = resources.GetString("rtxWithouExampleFa.Text");
+            // 
+            // rxtOnlyExample
+            // 
+            this.rxtOnlyExample.Location = new System.Drawing.Point(465, 379);
+            this.rxtOnlyExample.Name = "rxtOnlyExample";
+            this.rxtOnlyExample.Size = new System.Drawing.Size(257, 193);
+            this.rxtOnlyExample.TabIndex = 5;
+            this.rxtOnlyExample.Text = resources.GetString("rxtOnlyExample.Text");
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(783, 443);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 52);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(786, 515);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 47);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 696);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.rxtOnlyExample);
+            this.Controls.Add(this.rtxWithouExampleFa);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox rtxWithouExampleFa;
+        private System.Windows.Forms.RichTextBox rxtOnlyExample;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ C#/DictionaryCheker/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> allwords = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            if(op.ShowDialog()==DialogResult.OK)
+            {
+                string str = System.IO.File.ReadAllText(op.FileName);
+                str = str.Replace("\r\n", "\n");
+                str = str.Replace("\r", "");
+                string[] lines = str.Split('\n');
+                string[] linesfa = richTextBox2.Text.Replace("\r\n","\n").Split('\n');
+                string newfile = "";
+                string rtx = "";
+                string corrects = "";
+                string faults1 = "";
+                string faults2 = "";
+                string witoutexampleFa = "";
+                string onlyexamples = "";
+                int corrected = 0;
+                for (int i=0;i<lines.Length;i++)
+                {
+                    string line = lines[i];
+                    string[] items= line.Split('#');
+
+                    if(items.Length==7)
+                    {
+                        String word = items[0];
+                        String persian = items[1].Replace("،", ",");
+                        string newpersian = "";
+                        
+                        foreach (string s in persian.Split(','))
+                        {
+                            if (IsOnlyPersian(s))
+                            {
+                                if (newpersian.Length > 0) newpersian += "، ";
+                                newpersian += s.Trim() ;
+                            }
+                        }
+                        persian = newpersian;
+                        
+                        String example = items[2];
+                        String definition = items[3];
+                        String level = items[4];
+                        String pronounce = items[5];
+                        String examplefa = items[6];
+                        if(examplefa.Contains(word))
+                        {
+                            string pers = persian.Split('،')[0].Replace(" کردن", "").Replace(" شدن", "");
+                            if (pers.Length > 0)
+                            {
+                                examplefa = examplefa.Replace(word, pers);
+                                if (IsOnlyPersian(examplefa))
+                                {
+                                    corrected++;
+                                }
+                            }else
+                            {
+                                int a = 10;
+                            }
+                        }
+                        newfile = word + "#" + persian + "#" + example + "#" + definition + "#" + level + "#" + pronounce + "#" + examplefa;
+                        if (IsOnlyPersian(persian) && IsOnlyPersian(examplefa) && persian.Length>0)
+                        {
+                            //corrects += newfile + "\r\n";
+                            if(!allwords.Contains(word)) corrects += newfile + "\r\n";
+                        }
+                        else
+                        {
+                            if (IsOnlyPersian(persian) && persian.Length > 0)
+                            {
+                                witoutexampleFa += word + "#" + persian + "#" + example + "#" + definition + "#" + level + "#" + pronounce + "#" + "\r\n";
+                                onlyexamples += example + "\r\n";
+                            }
+                            else
+                            {
+                                faults1 += word + "\r\n";
+                            }
+                            //FindNonPersianParts(persian);
+                            //FindNonPersianParts(examplefa);
+                            //FindNonPersianParts(persian);
+                            //IsOnlyPersian(persian);
+                            //  bool isper = IsOnlyPersian(persian);
+                            //  bool isexa = IsOnlyPersian(examplefa);
+                            //  IsOnlyPersian(persian);
+
+
+                        }
+                        //
+                        //rtx += example + "\r\n";
+                    }
+                    else {
+                        faults2 += line.Split('#')[0] + "\r\n";
+                    }
+                    if (i % 50 == 0)
+                    {
+                        label1.Text = i + "/" + lines.Length;
+                        label1.Invalidate();
+                        Application.DoEvents();
+                    }
+                   
+                }
+                richTextBox1.Text = corrects;
+                richTextBox2.Text = faults1;
+                richTextBox3.Text = faults2;
+                rtxWithouExampleFa.Text = witoutexampleFa;
+                rxtOnlyExample.Text = onlyexamples;
+                //System.IO.File.WriteAllText(op.FileName.Replace(".txt","1.txt"), newfile);
+                MessageBox.Show("Finish corrected: " +corrected);
+                
+
+            }
+        }
+
+        static bool IsOnlyPersian(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return false;
+
+            // اگر رشته شامل حروف انگلیسی باشه
+            if (Regex.IsMatch(text, "[A-Za-z]")) return false;
+
+            if (FindNonPersianParts(text) > 2) return false;
+            // اجازه‌ی کاراکترهای فارسی، فاصله و علائم رایج
+            return true;
+        }
+
+        static int FindNonPersianParts(string text)
+        {
+            // فقط کاراکترهایی رو پیدا کن که بیرون از محدوده فارسی-عربی باشن
+            var matches = Regex.Matches(text, @"[^\u0600-\u06FF\u200C\s،؛؟٪ءآأإئًٌٍَُِّ.!؟(){}\[\]«»""]");
+            return matches.Count;
+            //if (matches.Count == 0)
+            //{
+            //    Console.WriteLine("✅ متن فقط فارسی است");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("❌ متن حاوی کاراکترهای غیر فارسی است");
+            //    foreach (Match match in matches)
+            //    {
+            //        Console.WriteLine($"Nob persian:'{match.Value}'in position {match.Index}");
+            //    }
+            //}
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string complete = "";
+            for(int i=0;i<rtxWithouExampleFa.Lines.Length;i++)
+            {
+                string com = rtxWithouExampleFa.Lines[i]+ rxtOnlyExample.Lines[i];
+                complete += com + "\r\n";
+
+            }
+            rtxWithouExampleFa.Text = complete;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
